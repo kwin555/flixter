ae447435fa285c6623162c39cd7844c6edb1d73a
kwin555 <ktn555@att.net>
2018-01-30 16:19:54 +0000

    v12 added instructor course validations for title, cost and description simple form

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 4 }
+  validates :description, presence: true, length: { minimum: 5 }
+  validates :cost, presence: true, numericality: { greater_than_or_equal_to: 0}
+
 end
