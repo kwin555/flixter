e2b2d385f420d46acdaf04e1b5c8fa08b19de3a7
kwin555 <ktn555@att.net>
2018-01-30 18:06:52 +0000

    v15 added sections to web app

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
+  has_many :sections
 
   validates :title, presence: true, length: { minimum: 4 }
   validates :description, presence: true, length: { minimum: 5 }
