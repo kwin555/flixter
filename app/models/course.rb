f5fdbe90e4952010327af7e1deab523debd3cace
kwin555 <ktn555@att.net>
2018-02-06 23:02:39 +0000

    v17 added stripe payments to web app

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,4 +8,12 @@ class Course < ApplicationRecord
   validates :title, presence: true, length: { minimum: 4 }
   validates :description, presence: true, length: { minimum: 5 }
   validates :cost, presence: true, numericality: { greater_than_or_equal_to: 0}
+
+  def free?
+    cost.zero?
+  end
+
+  def premium?
+    ! free?
+  end
 end
